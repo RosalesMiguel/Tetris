30b26b0c0fe53aacfc272baa0dc24d019bb04833
RosalesMiguel <m.rosales.mar123@gmail.com>
2021-06-21 15:35:50 +0800

    Update mainmenu.cpp

--- mainmenu.cpp
+++ mainmenu.cpp
@@ -1,137 +1,135 @@
-#include <cstdlib>
 #include <iostream>
 #include <stdio.h>
-#include <windows.h>
-#include "MMfunction.h"
+#include <conio.h>
+#include <math.h>
+#define ESC 27
+//for reference
+/*#define UP 72
+#define DOWN 80
+#define LEFT 75
+#define RIGHT 77*/
 
-int opc=0;
+using namespace std;
 
-void Start();
-int HowToPlay();
-int Story();
-void Leaderboard();
-
-MENU myMenu;
-
-int main(int argc, char *argv[]) {
-  bool Exit=false;
-
-  myMenu.Add_Items("1. START",1);
-  myMenu.Add_Items("2. STORY",2);
-  myMenu.Add_Items("3. HOW TO PLAY", 3);
-  myMenu.Add_Items("4. LEADERBOARD",4);
-  myMenu.Add_Items("5. EXIT",5);
-
-  myMenu.Set_Space(2);
-
-do {
-	/* TextColor(10);
-	Say(33,2,"PRISON TETRIS"); */
-
-	if(opc==0) {
-        TextColor(10);
-	    Say(33,2,"PRISON TETRIS");
-		SetColorB(2,19);
-		Rectangle(20,8,40,12," ");
-		SetColorA(2,22);
-		Say(30,10,"WELCOME TO INFERNO!");
-  		opc=myMenu.Draw(33,12);
-	    }
-
-   switch (opc) {
-
-			case 1:
-			   Cls();
-			   Start();
-			break;
-
-			case 2:
-				Cls();
-				Story();
-			break;
-
-			case 3:
-				Cls();
-				HowToPlay();
-			break;
-
-			case 4:
-                Cls();
-                Leaderboard();
-            break;
-
-			case 5:
-				Exit=true;
-				std::cout<<"\n";
-				std::cout<<"\n";
-			break;
-
-	  }
-
-
-} while(Exit==false);
-
-  return 0;
-;
-
+// in case needed
+/*void Draw(){
+    cout << "PRISONER TETRIS" << endl;
+    cout << "MAIN MENU" << endl;
+    cout << "Use UP and DOWN arrow keys to control main menu. Press ENTER to select. Press ESC to exit." << endl;
+    cout << "..............." << endl;
+    cout << ".....Start....." << endl;
+    cout << "..............." << endl;
+    cout << "..............." << endl;
+    cout << "..How to Play.." << endl;
+    cout << "..............." << endl;
+    cout << "..............." << endl;
+    cout << ".....Story....." << endl;
+    cout << "..............." << endl;
+}*/
+void chooseStart(){
+    cout << "PRISONER TETRIS" << endl;
+    cout << "MAIN MENU" << endl;
+    cout << "Use UP and DOWN arrow keys to control main menu. Press ENTER to select. Press ESC to exit." << endl;
+    cout << "###############" << endl;
+    cout << "##...Start...##" << endl;
+    cout << "###############" << endl;
+    cout << "..............." << endl;
+    cout << "..How to Play.." << endl;
+    cout << "..............." << endl;
+    cout << "..............." << endl;
+    cout << ".....Story....." << endl;
+    cout << "..............." << endl;
+}
+void chooseHowtoPlay(){
+    cout << "PRISONER TETRIS" << endl;
+    cout << "MAIN MENU" << endl;
+    cout << "Use UP and DOWN arrow keys to control main menu. Press ENTER to select. Press ESC to exit." << endl;
+    cout << "..............." << endl;
+    cout << ".....Start....." << endl;
+    cout << "..............." << endl;
+    cout << "###############" << endl;
+    cout << "##How to Play##" << endl;
+    cout << "###############" << endl;
+    cout << "..............." << endl;
+    cout << ".....Story....." << endl;
+    cout << "..............." << endl;
+}
+void chooseStory(){
+    cout << "PRISONER TETRIS" << endl;
+    cout << "MAIN MENU" << endl;
+    cout << "Use UP and DOWN arrow keys to control main menu. Press ENTER to select. Press ESC to exit." << endl;
+    cout << "..............." << endl;
+    cout << ".....Start....." << endl;
+    cout << "..............." << endl;
+    cout << "..............." << endl;
+    cout << "..How to Play.." << endl;
+    cout << "..............." << endl;
+    cout << "###############" << endl;
+    cout << "##...Story...##" << endl;
+    cout << "###############" << endl;
 }
 
-void Start() {
+int main(){
+    int choice = 1;
+    int ch;
+    chooseStart();
+    while ((ch = getch()) != ESC){
+        if(ch == 72){
+                choice--;
+                if (choice == 0) choice = 1;
+            }
+        else if(ch == 80){
+                choice++;
+                if (choice == 4) choice = 3;
+        }
+        if(choice == 1){
+            system("cls");
+            chooseStart();
+        }
+        else if(choice == 2){
+            system("cls");
+            chooseHowtoPlay();
+        }
+        else if(choice == 3){
+            system("cls");
+            chooseStory();
+        }
+        //If ENTER was pressed
+        if (ch == 13) {
+            if(choice == 1){
+                system("cls");
+                cout << "wala pa na butang" << endl;
+            }
+            else if(choice == 2){
+                system("cls");
+                char buffer[500];
+                FILE *fp = fopen("howtoplay.txt", "r");
+                if(!fp){
+                    cout << "File howtoplay.txt not found" << endl;
+                    return -1;
+                }
+                for(int i = 0; i < 31; i++){
+                    fgets(buffer, 500, fp);
+                    cout << buffer;
+                }
+            }
 
-}
+            else if(choice == 3){
+                system("cls");
+                char buffer[500];
+                FILE *fp = fopen("bgstory.txt", "r");
+                if(!fp){
+                    cout << "File bgstory.txt not found" << endl;
+                    return -1;
+                }
+                 for(int i = 0; i < 9; i++){
+                    fgets(buffer, 500, fp);
+                    cout << buffer;
+                }
+            }
 
-int HowToPlay() {
-  TextColor(12); /*
-  Rectangle(20,8,40,12,"%");
-
-  SetColorA(2,22);
-  Say(31,10,"*** HOW TO PLAY ***"); */
-    const char* filename = "howtoplay.txt";
-    FILE *fp = fopen(filename, "r");
-    if (fp == NULL) {
-        printf("Error: could not open file %s", filename);
-        return 1;
+            }
     }
-    char ch;
-    while ((ch = fgetc(fp)) != EOF)
-        putchar(ch);
-    fclose(fp);
-    int tec=getch();
-    opc=0;
+    cout << "The program is now exiting" << endl;
     return 0;
 }
-
-int Story() {
-  TextColor(12);
-  // Rectangle(20,8,40,12,"x");
-
-  SetColorA(2,22);
-  /* Say(31,10,"*** STORY ***");
-
-  int tec=getch();
-  opc=0; */
-    const char* filename = "bgstory.txt";
-    FILE *fp = fopen(filename, "r");
-    if (fp == NULL) {
-        printf("Error: could not open file %s", filename);
-        return 1;
-    }
-    char ch;
-    while ((ch = fgetc(fp)) != EOF)
-        putchar(ch);
-    fclose(fp);
-    int tec=getch();
-    opc=0;
-    return 0;
-}
-
-void Leaderboard() {
-  TextColor(12);
-  Rectangle(20,8,40,12,":");
-
-  SetColorA(2,22);
-  Say(31,10,"*** LEADERBOARD ***");
-  int tec=getch();
-  opc=0;
-}
-
